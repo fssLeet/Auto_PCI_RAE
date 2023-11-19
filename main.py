c6837c76090f0ad61f09810cca56256f9264d3a0
Fernando <fst.engenharia@gmail.com>
2023-11-19 20:21:42 -0300

    update

--- main.py
+++ main.py
@@ -3,49 +3,13 @@ from tkinter import messagebox
 import time
 import xlwings as xw
 import cellMap
+import messages
 
-print(
-    """
-                                        
-      .:::::::::::.      :------------  
-       :===========:   .=***********+.  
-        :===========: -************-    
-         .============***********=      
-          .===========+********+:       
-            ============******-         
-             -===========+**+.          
-              ::::::::::::-:            
-            .::::::::::::.              
-           -************+=:             
-         .+***********+====-            
-        =************+=======           
-      :+***********+==========.         
-    .=***********+:============:        
-   -************-   -===========:       
-  =+++++++++++=.     :-----------:      
-                                        
-                                       
--[ Cópia Automática entre PCI e RAE ]-
-
-Versão: 0.1
-Data: 17/11/2023
-Dúvidas e problemas? envie e-mail para: fst.engenharia@gmail.com
-Autor: FS
-
-Download, Atualizações e FAQ: https://github.com/fssLeet/Auto_PCI_RAE
-      """
-)
+print(messages.inicio)
 
 messagebox.showwarning(
     "Atenção!",
-    """
-Como usar o programa:
-1-Passo: Selecione a PCI (compatível com versões 2022 e 2023)
-2-Passo: Selecione a RAE (compatível apenas com a versão 11-AGO-2023)
-3-Passo: Clique em "Fim" na mensagem de erro da RAE
-
-Atenção: Após a cópia automática confira todos o dados!
-""",
+    messages.aviso,
 )
 
 # Caminhos do arquivo da PCI e RAE
---
+++ messages.py
@@ -0,0 +1,38 @@
+inicio = """
+                                        
+      .:::::::::::.      :------------  
+       :===========:   .=***********+.  
+        :===========: -************-    
+         .============***********=      
+          .===========+********+:       
+            ============******-         
+             -===========+**+.          
+              ::::::::::::-:            
+            .::::::::::::.              
+           -************+=:             
+         .+***********+====-            
+        =************+=======           
+      :+***********+==========.         
+    .=***********+:============:        
+   -************-   -===========:       
+  =+++++++++++=.     :-----------:      
+                                        
+                                       
+-[ Cópia Automática entre PCI e RAE ]-
+
+Versão: 0.1
+Data: 17/11/2023
+Dúvidas e problemas? envie e-mail para: fst.engenharia@gmail.com
+Autor: FS
+
+Download, Atualizações e FAQ: https://github.com/fssLeet/Auto_PCI_RAE
+      """
+
+aviso = """
+Como usar o programa:
+1-Passo: Selecione a PCI (compatível com versões 2022 e 2023)
+2-Passo: Selecione a RAE (compatível apenas com a versão 11-AGO-2023)
+3-Passo: Clique em "Fim" na mensagem de erro da RAE
+
+Atenção: Após a cópia automática confira todos o dados!
+"""
